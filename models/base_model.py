52f3956aa253e1a6ccb8190bf3bd053f56b4f66a
jozsa <allisonjoyweiner@gmail.com>
2019-02-14 22:41:44 +0000

    Implemented working creation of BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -6,9 +6,17 @@ class BaseModel:
     """ base class model for """
     def __init__(self, *args, **kwargs):
         """ initializes class with id and created at time"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key is 'created_at' or key is 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f") 
+                elif key is '__class__':
+                    value = self.__class__
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
 
     def __str__(self):
         """ prints out a string representation of called instance """
@@ -18,11 +26,9 @@ class BaseModel:
         """ updates the current instance """
         self.updated_at = datetime.today()
 
-    def to_dict(self, **kwargs):
+    def to_dict(self):
         """ makes instance a dictionary """
-        for key, value in kwargs.items():
-            if key is 'created_at' or key is 'updated_at':
-                value = datetime(value)
-            setattr(self, key, value)
+        self.__dict__['__class__'] = self.__class__.__name__
+        self.__dict__['created_at'] = datetime.isoformat(self.created_at)
+        self.__dict__['updated_at'] = datetime.isoformat(self.updated_at)
         return self.__dict__
-
