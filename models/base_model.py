44537fc348323a4bf0274ad96a4f9ab1d18d5a61
mKleinCreative <miketheklein@gmail.com>
2019-02-14 00:14:09 +0000

    pushing up latest refactors to make things more dynamic

--- models/base_model.py
+++ models/base_model.py
@@ -13,14 +13,14 @@ class BaseModel:
     def __str__(self):
         """ prints out a string representation of called instance """
         return "[{}] ({} <{}>".format(
-                        'BaseModel', self.id, self.__dict__)
+                        type(self).__name__, self.id, self.__dict__)
     def save(self):
         """ updates the current instance """
         self.updated_at = datetime.today()
 
     def to_dict(self):
         """ makes instance a dictionary """
-        self.__dict__['__class__'] = 'BaseModel'
+        self.__dict__['__class__'] = type(self).__name__
         self.__dict__['created_at'] = datetime.isoformat(self.created_at)
         self.__dict__['updated_at'] = datetime.isoformat(self.updated_at)
         return self.__dict__
