60281552c8b325581d47fa3ca46f9de85a0143d1
mKleinCreative <miketheklein@gmail.com>
2019-02-14 00:02:22 +0000

    added in functionality and have a working BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -4,40 +4,24 @@ from datetime import datetime
 
 class BaseModel:
     """ base class model for """
-    def __init__(self):
+    def __init__(self, id=None, created_at=0, updated_at=0):
         """ initializes class with id and created at time"""
-        self.id = id
-        self.created_at = created_at
-        self.updated_at = updated_at
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
 
     def __str__(self):
         """ prints out a string representation of called instance """
-        return ("[{}] ({} <{}>".format(
-                        self.__class__, self.id, self.__dict__)
+        return "[{}] ({} <{}>".format(
+                        'BaseModel', self.id, self.__dict__)
     def save(self):
         """ updates the current instance """
-        return self.updated_at = updated_at()
-
-
-    @property
-    def id(self):
-        return self.id
-
-    @id.setter
-    def id(self):
-        self.id = str(uuid.uuid4())
-
-    @property
-    def created_at(self):
-        return self.updated_at()
-
-    @created_at.setter
-    def created_at(self):
-        self.created_at = datetime.today()
-
-    @property
-    def updated_at(self):
-        return self.updated_at()
-    @updated_at.setter
-    def updated_at(self):
         self.updated_at = datetime.today()
+
+    def to_dict(self):
+        """ makes instance a dictionary """
+        self.__dict__['__class__'] = 'BaseModel'
+        self.__dict__['created_at'] = datetime.isoformat(self.created_at)
+        self.__dict__['updated_at'] = datetime.isoformat(self.updated_at)
+        return self.__dict__
+
