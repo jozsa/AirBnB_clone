1866ca54f0b2eff0d8595361475a608dd12bc79a
mKleinCreative <miketheklein@gmail.com>
2019-02-14 20:55:21 +0000

    pushing up cmdline for testing

--- console.py
+++ console.py
@@ -1,2 +1,23 @@
 #!/bin/usr/python3
-from cmd import cmd
+import cmd
+import readline
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    def do_EOF(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def emptyline(self):
+        """returns another empty line if an emptyline is passed in
+            Otherwise it'd repeat the last command given
+            """
+        pass
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
+
