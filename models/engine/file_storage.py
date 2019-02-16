4a506f452d1244b8b85d72386ab0e09c3401af32
jozsa <allisonjoyweiner@gmail.com>
2019-02-16 00:20:42 +0000

    File storage functionality up. Serialization and deserialization of object/JSON file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,35 +4,40 @@ This module has one class: FileStorage
 FileStorage will be able to serialize instances
 to JSON file & deserialize JSON file to instances
 """
-import models
+from models.base_model import BaseModel
 import json
+
 
 class FileStorage:
     """
     """
-    def __init__(self):
-        """ """
-        self.__file_path = 'file.json'
-        self.__objects = {}
+    __file_path = 'file.json'
+    __objects = {}
 
     def all(self):
         """ Returns the dictionary __objects """
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """ Sets 'obj' in __objects with key <obj class name>.id """
-        self.key = obj.__class__.__name__ + '.' + obj.id
-        self.__objects[self.key] = obj.to_dict()
+        key = obj.__class__.__name__ + '.' + obj.id
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """ Serializes __objects to JSON file """
-        with open(self.__file_path, mode='w+', encoding='utf-8') as json_file:
-            json.dump(self.__objects, json_file)
+        for key, value in FileStorage.__objects.items():
+            if not isinstance(value, dict):
+                FileStorage.__objects[key] = value.to_dict()
+        with open(FileStorage.__file_path,
+                  mode='w+', encoding='utf-8') as json_file:
+            json.dump(FileStorage.__objects, json_file)
 
     def reload(self):
         """Deserializes JSON file to __objects if file path exists"""
         try:
-            with open(self.__file_path, encoding='utf-8') as json_file:
-                self.__objects = json.load(json_file)
+            with open(FileStorage.__file_path, encoding='utf-8') as json_file:
+                FileStorage.__objects = json.load(json_file)
+            for value in FileStorage.__objects.values():
+                value = self.new(BaseModel(**value))
         except FileNotFoundError:
             pass
