f84e98225468908a224319ecabaffb60033cfd19
jozsa <allisonjoyweiner@gmail.com>
2019-02-18 03:41:33 +0000

    Review subclass of BaseModel

---
+++ models/review.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module has one class: Review
+inherited from BaseModel
+"""
+from models.base_mdoel import BaseModel
+
+
+class Review(BaseModel):
+    """Review class:
+
+    Attributes:
+        place_id: str, State.id
+        user_id: str, User.id
+        text: str, text containing the review
+    """
+    place_id = ''
+    user_id = ''
+    text = ''
