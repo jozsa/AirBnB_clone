25d6c91fea9d011200d0d171b4fcc8de964fb067
jozsa <allisonjoyweiner@gmail.com>
2019-02-18 06:07:07 +0000

    Add more class imports, 'City' to classes list

--- console.py
+++ console.py
@@ -10,6 +10,11 @@ import json
 import models
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from models.engine.file_storage import FileStorage
 
 
@@ -28,6 +33,7 @@ class HBNBCommand(cmd.Cmd):
         'BaseModel',
         'User',
         'State',
+        'City',
         'Amenity',
         'Place',
         'Review'
