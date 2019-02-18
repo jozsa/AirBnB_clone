6c423ef14c1f9aaf833fedcf65a7b5902a273b5a
jozsa <allisonjoyweiner@gmail.com>
2019-02-18 03:22:17 +0000

    Documentation for class

--- models/user.py
+++ models/user.py
@@ -7,7 +7,14 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """ """
+    """User class:
+
+    Attributes:
+        email: str, email address
+        password: str, password
+        first_name: str, first name of user
+        last_name: str, last name of user
+    """
     email = ''
     password = ''
     first_name = ''
