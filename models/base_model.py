657ab71103c947114cd9c9d8f3f70d3f192509b6
jozsa <allisonjoyweiner@gmail.com>
2019-02-18 03:07:44 +0000

    PEP8 compliance

--- models/base_model.py
+++ models/base_model.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
+"""
+This module has one class: BaseModel
+BaseModel will act as the base class
+for all future sub-classes of the
+AirBnB clone.
+"""
 import uuid as uuid
 from datetime import datetime
 import models
 
+
 class BaseModel:
-    """ base class model for """
+    """ base class model for AirBnB objects
+
+    Attributes:
+        id: str, unique UUID of instance
+        created_at: datetime, time of instance creation
+        updated_at: datetime, instance update time
+    """
     def __init__(self, *args, **kwargs):
         """ initializes class with id and created at time"""
         self.id = str(uuid.uuid4())
@@ -13,7 +26,8 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
-                    self.__dict__[key] = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    self.__dict__[key] = \
+                        datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 else:
                     self.__dict__[key] = value
         else:
@@ -23,6 +37,7 @@ class BaseModel:
         """ prints out a string representation of called instance """
         return "[{}] ({}) {}".format(
                     self.__class__.__name__, self.id, self.__dict__)
+
     def save(self):
         """ updates the current instance """
         self.updated_at = datetime.today()
