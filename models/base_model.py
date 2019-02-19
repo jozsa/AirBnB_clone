34667534e3c7a23b9e59254f3971ebd3387a12e7
mKleinCreative <miketheklein@gmail.com>
2019-02-19 01:57:13 +0000

    fixing small errors

--- models/base_model.py
+++ models/base_model.py
@@ -53,4 +53,4 @@ class BaseModel:
         json_dict['__class__'] = self.__class__.__name__
         json_dict['created_at'] = self.created_at.isoformat()
         json_dict['updated_at'] = self.updated_at.isoformat()
-      return json_dict
+        return json_dict
