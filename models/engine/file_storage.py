4262d9ec0ebdb01acf4733c9d11d4271c8fa25ed
jozsa <allisonjoyweiner@gmail.com>
2019-02-25 20:26:17 +0000

    Deleting Mike's files to create my own

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -47,7 +47,5 @@ class FileStorage:
             for value in FileStorage.__objects.copy().values():
                 classname = value['__class__']
                 self.new(eval(classname)(**value))
-                print('added obj')
-                print(self.__objects)
         except FileNotFoundError:
             pass
